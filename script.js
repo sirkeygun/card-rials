2e0e86bd68f04575403cdefbb0725c49b4ae1a98
kevin kinyua <51110607+sirkeygun@users.noreply.github.com>
2022-05-15 17:15:03 +0300

    adde files

---
+++ script.js
@@ -0,0 +1,16 @@
+import Deck from "./deck.js" //import Deck 
+
+
+
+
+
+startGame()
+function startGame() {
+    const deck = new Deck() //create a new deck
+    deck.shuffle()   //shuffle the deck
+
+
+
+}
+
+
